eca3c7bf5132f866fa86cce167797a351bad966f
ivan_omgru <loverussian62@gmail.com>
2026-07-29 16:19:21 +0300

    Add files via upload

---
+++ Mypageruir/assets/js/video/config.js
@@ -0,0 +1,7 @@
+// ==================== تنظیمات وضعیت لایو ====================
+// 0 = غیرفعال (هیچ وقت لایو نشان داده نشود)
+// 1 = فعال (همیشه لایو نشان داده شود، بدون در نظر گرفتن زمان)
+// 2 = بر اساس زمان (با startTime مقایسه شود)
+export const LIVE_STATUS = 2;
+export const liveClasses = {"class1": {startTime: new Date("2027-05-03T15:00:00"),teacherFa: "استاد - ivan_omgru",teacherRu: "Преподаватель - ivan_omgru",imgFa: "Mypageruir/assets/images/resources/live-class-1-1.jpg",imgRu: "/Mypageruir/assets/images/resources/live-class-1-1.jpg",
+liveLink: "https://www.aparat.com/video/video/embed/videohash/knq8lfb/vt/frame"}};
