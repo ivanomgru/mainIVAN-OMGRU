167a9cffd33ba73cb3cc44f234d33c85b8fbbd4d
ivan_omgru <loverussian62@gmail.com>
2026-08-14 19:56:46 +0330

    Delete Mypageruir/assets/js/video directory

--- Mypageruir/assets/js/video/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-import { initLiveManager } from './live-manager.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    // فقط مدیریت لایو (کلاس‌های زنده) فعال می‌مونه
-    initLiveManager();
-    // video-player و controls-hide حذف شدند چون Plyr جایگزین شده
-});
